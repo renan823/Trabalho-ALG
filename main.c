6e552922bc4a0876d3c83c17c90ef0a1d4b01c8f
Augusto <afildefonso16@gmail.com>
2024-12-06 11:50:58 -0300

    comentando main.c

--- main.c
+++ main.c
@@ -1,30 +1,44 @@
+/**
+ * Projeto 2 - Estruturas de Dados e Algoritmos
+ *
+ * @author Augusto Fernandes Ildefonso
+ * @author Renan Trofino
+ */
 #include "set.h"
 #include "item.h"
 #include <stdio.h>
 
+/**
+ * Função principal
+ *
+ * @return Retorna 0 se for bem sucedido
+ */
 int main(void) {
     int ed;  // 0 para avl, 1 para rubro-negro
     int tam_set, tam_set2, chave;
     int op, set_alvo;
 
-    scanf("%d", &ed);
+    scanf("%d", &ed); // Recebe o tipo de árvore que o set vai utilizar
 
     SET *A = set_criar(ed);
     SET *B = set_criar(ed);
     SET *C = NULL;
 
-    scanf("%d %d", &tam_set, &tam_set2);
+    scanf("%d %d", &tam_set, &tam_set2); // Lê o tamanho de cada set
 
+    // Recebe valores do set 1
     for(int i = 0; i < tam_set; i++){
         scanf("%d", &chave);
         set_inserir(A, item_criar(chave, NULL));
     }
 
+    // Recebe valoes do set 2
     for(int i = 0; i < tam_set2; i++){
         scanf("%d", &chave);
         set_inserir(B, item_criar(chave, NULL));
     }
 
+    // Lê a operação que será feita
     scanf("%d", &op);
 
     switch(op) {
